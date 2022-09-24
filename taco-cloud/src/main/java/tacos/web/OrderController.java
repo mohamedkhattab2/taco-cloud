f1a6e4bf572c02279feee366130b7c419c39aac9
mohamedkhattab2 <mohamed.khattab84@yahoo.com>
2022-09-24 18:02:00 +0200

    add orderForm template

--- taco-cloud/src/main/java/tacos/web/OrderController.java
+++ taco-cloud/src/main/java/tacos/web/OrderController.java
@@ -3,8 +3,11 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import tacos.TacoOrder;
 
 @Slf4j
 @Controller
@@ -14,5 +17,12 @@ public class OrderController {
     @GetMapping("current")
     public String orderForm(){
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus){
+        log.info("orderSubmitted: {}",order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
